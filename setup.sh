5f6b73613b532a89fe6f56434ce7a68f26a67450
Oliver Hamlet <git@ortham.net>
2026-02-08 18:01:13 +0000

    Add a setup script
    That runs most of the other scripts.

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e -o pipefail
+
+cd flatpak
+./install.sh
+cd -
+
+if [ "$XDG_SESSION_DESKTOP" = "gnome" ]
+then
+	cd gnome/nvidia-sleep
+	sudo ./install.sh
+	cd ..
+	./install-extensions.sh
+	cd ..
+fi
+
+cd kernel
+./configure-kernel-modules.sh
+cd -
+
+cd syncthing
+./install.sh
+cd -
+
+cd tools
+./install-git-credential-manager.sh
+./install-hugo.sh
+./install-uv.sh
+./install-yubico-authenticator.sh
+cd -
